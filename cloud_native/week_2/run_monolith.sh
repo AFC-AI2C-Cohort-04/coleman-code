d9a0d14a5e1e026f3aa09e6bb2affd596b8bd5a6
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2024-01-23 17:54:56 -0500

    Create run_monolith.sh

---
+++ cloud_native/week_2/run_monolith.sh
@@ -0,0 +1,8 @@
+export MYSQL_HOST=$(terraform output -raw mysql_fqdn)
+export MYSQL_USER=$(terraform output -raw mysql_admin_username)
+export MYSQL_PASSWORD=$(terraform output -raw mysql_admin_password)
+export SPRING_REDIS_HOST=$(terraform output -raw redis_hostname)
+export SPRING_REDIS_PASSWORD=$(terraform output -raw redis_primary_access_key)
+export SPRING_REDIS_PORT=$(terraform output -raw redis_port)
+cd /home/packer/cloudchat/task1-monolith/
+/bin/java -jar ./target/cloudchat-1.0.0.jar
