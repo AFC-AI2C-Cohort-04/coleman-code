c7688f1eb9613e91f787190211262aa792510e5f
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2024-01-25 15:43:39 -0500

    Create run_monolith.sh

---
+++ cloud_native/week_2/task_1_packer_files/run_monolith.sh
@@ -0,0 +1,7 @@
+export MYSQL_HOST="shared-mysql-fs-dnigtfhq.mysql.database.azure.com"
+export MYSQL_USER="Monolithic_CloudChat_123"
+export MYSQL_PASSWORD="Monolithic_CloudChat_123"
+export SPRING_REDIS_HOST="redis-cache-rkgqfapa.redis.cache.windows.net"
+export SPRING_REDIS_PORT="6379"
+export SPRING_REDIS_PASSWORD="gHkEOlaYhIOrp7EGp1ZX0nWMZdFU6iOKRAzCaF5zzWM="
+/bin/java -jar /tmp/cloudchat-1.0.0.jar
