804217e00ca3aa30007763fee21cd2a07f35d22b
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2024-01-25 19:17:51 -0500

    Update run_monolith.sh

--- cloud_native/week_2/task_1_packer_files/run_monolith.sh
+++ cloud_native/week_2/task_1_packer_files/run_monolith.sh
@@ -1,8 +1,8 @@
-export MYSQL_HOST="shared-mysql-fs-dnigtfhq.mysql.database.azure.com"
+export MYSQL_HOST="shared-mysql-fs-fflgmddz.mysql.database.azure.com"
 export MYSQL_USER="Monolithic_CloudChat_123"
 export MYSQL_PASSWORD="Monolithic_CloudChat_123"
-export SPRING_REDIS_HOST="redis-cache-rkgqfapa.redis.cache.windows.net"
+export SPRING_REDIS_HOST="redis-cache-vwuhyibe.redis.cache.windows.net"
 export SPRING_REDIS_PORT="6379"
-export SPRING_REDIS_PASSWORD="gHkEOlaYhIOrp7EGp1ZX0nWMZdFU6iOKRAzCaF5zzWM="
-cd /home/packer/
+export SPRING_REDIS_PASSWORD="u96gLdbzaA7CQ6OMVzQWLPPmIib2AudURAzCaHF9L7Y="
+cd /home/packer
 /bin/java -jar ./target/cloudchat-1.0.0.jar
