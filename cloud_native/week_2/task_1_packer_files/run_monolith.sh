b7102f85ddcc588790c818681c4cae9974b40606
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2024-01-27 01:11:10 -0500

    Delete cloud_native/week_2/task_1_packer_files directory

--- cloud_native/week_2/task_1_packer_files/run_monolith.sh
+++
@@ -1,8 +0,0 @@
-export MYSQL_HOST="shared-mysql-fs-fflgmddz.mysql.database.azure.com"
-export MYSQL_USER="Monolithic_CloudChat_123"
-export MYSQL_PASSWORD="Monolithic_CloudChat_123"
-export SPRING_REDIS_HOST="redis-cache-vwuhyibe.redis.cache.windows.net"
-export SPRING_REDIS_PORT="6379"
-export SPRING_REDIS_PASSWORD="u96gLdbzaA7CQ6OMVzQWLPPmIib2AudURAzCaHF9L7Y="
-cd /home/packer
-/bin/java -jar ./target/cloudchat-1.0.0.jar
